dc67add1bb52be3252d254bf916cddf099e3afd8
Sam Dillard <samdillard@Sams-MacBook-Pro.local>
2018-10-09 17:27:46 -0700

    updated readme with cluster diagram, updated cluster.sh, .gitignore

--- cluster.sh
+++ cluster.sh
@@ -14,3 +14,15 @@ docker exec meta1 influxd-ctl add-data data1:8088
 docker exec meta1 influxd-ctl add-data data2:8088
 
 echo "Cluster successfully created"
+
+if [ $(uname) == "Darwin" ]; then
+  open http://localhost:3010
+  open http://localhost:8888
+elif [ $(uname) == "Linux" ]; then
+  sensible-browser http://localhost:8888
+  sensible-browser http://localhost:3010
+else
+  echo "no browser detected..."
+fi
+
+docker exec meta1 bash -c "influxd-ctl show"
